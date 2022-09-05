a21ca7bdbc817cb7d76e80e1bc93ab4ab4b3c170
Aviril <89867985+Aviril@users.noreply.github.com>
2022-09-05 16:58:50 +0300

    Update vcl.js

--- src/vcl.js
+++ src/vcl.js
@@ -144,7 +144,7 @@ CVCL.private.parseObject = (parser, buffer, rw, isChild) => {
         else {
             if (parser.isTypeID && CVCL.private.isVoid(parser.index) && (rw == CVCL.private.types.init)) parser.index = String(parser.pointer.length() + 1)
             if (!CVCL.private.isVoid(parser.index)) {
-                if (parser.isTypeID && (rw == CVCL.private.types.newline)) parser.pointer.set(parser.pointer.length() + 1, parser.index)
+                if (parser.isTypeID && (rw == CVCL.private.types.newline)) parser.pointer.set(parser.pointer.length(), parser.index)
                 else if (rw == CVCL.private.types.init) {
                     const [line, lineText] = CVCL.private.fetchLine(vKit.String.sub(buffer, 0, parser.ref))
                     const indexTypePadding = (parser.isTypeID && (parser.ref - parser.isTypeID - 1)) || 0
