20182336ec682589646d3686cc68e939f373ee73
Aviril <89867985+Aviril@users.noreply.github.com>
2022-09-06 15:46:09 +0300

    Made vid classless

--- src/vid.js
+++ src/vid.js
@@ -13,46 +13,41 @@
 //////////////
 
 const vKit = require(".")
+const private = {}
 
 
 /////////////////
 // Class: VID //
 /////////////////
 
-const CVID = vKit.Class()
-vKit.vid = CVID.public
-CVID.private.buffer = vKit.Object()
-CVID.private.counter = 0
+vKit.vid = {}
+private.buffer = {}
+private.counter = 0
 
-
-/////////////////////
-// Static Members //
-/////////////////////
-
-// @Desc: Creates a unique VID
-CVID.public.addMethod("create", () => {
+// @Desc: Creates a unique vid
+vKit.vid.create = () => {
     var vid = false
     while(!vid) {
-        const vvid = vKit.toBase64(vKit.crypto.getRandomValues(new Uint8Array(8)).join("") + (Date.now() + CVID.private.counter))
-        if (CVID.public.blacklist(vvid)) vid = vvid
-        CVID.private.counter += 1
+        const vvid = vKit.toBase64(vKit.crypto.getRandomValues(new Uint8Array(8)).join("") + (Date.now() + private.counter))
+        if (vKit.vid.blacklist(vvid)) vid = vvid
+        private.counter += 1
     }
     return vid
-})
+}
 
-// @Desc: Blacklists a VID
-CVID.public.addMethod("blacklist", (vid) => {
-    if (!vKit.isString(vid) || CVID.private.buffer.get(vid)) return false
-    CVID.private.buffer.set(vid, true)
+// @Desc: Blacklists a vid
+vKit.vid.blacklist = (vid) => {
+    if (!vKit.isString(vid) || private.buffer[vid]) return false
+    private.buffer[vid] = true
     return true
-})
+}
 
-// @Desc: Assigns/Fetches VID on/from valid instance
-CVID.public.addMethod("fetch", (parent, assignVID, isReadOnly) => {
+// @Desc: Assigns/Fetches vid on/from instance
+vKit.vid.fetch = (parent, assignVID, isReadOnly) => {
     if (vKit.isNull(parent) || vKit.isBool(parent) || vKit.isString(parent) || vKit.isNumber(parent)) return false
     parent.prototype = parent.prototype || {}
     if (!isReadOnly && !parent.prototype.vid) {
         Object.defineProperty(parent.prototype, "vid", {value: assignVID || `${vKit.identifier}:${vKit.vid.create()}`, enumerable: true, configurable: false, writable: false})
     }
     return parent.prototype.vid
-})
+}
