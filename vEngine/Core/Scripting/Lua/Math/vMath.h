ce5d2479269e104a9c7a8569346f103773f4c6e4
A <48655846+ov-tron@users.noreply.github.com>
2026-02-01 03:19:37 +0300

    Added modules

--- vEngine/Core/Scripting/Lua/Math/vMath.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "vCommon.h"
-#include "Core/Scripting/Lua/vLuaBooter.h"
-#include "Core/Helpers/wiMath.h"
-
-// Boot Modules
-#include "Core/Scripting/Lua/Math/Vector/vVector.h"
-#include "Core/Scripting/Lua/Math/Matrix/vMatrix.h"
--- vEngine/Core/Scripting/Lua/vLuaBooter.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-#include "Core/Scripting/Lua/vLua.h"
-#include "Core/Scripting/Lua/vLuaManager.h"
