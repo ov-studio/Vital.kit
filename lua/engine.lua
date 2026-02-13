9ecf8009a3fe7a8a9b9cba914c321781b9303fbc
A <103286142+ov-anisa@users.noreply.github.com>
2026-02-13 19:46:46 +0300

    Update engine.lua

--- lua/engine.lua
+++ lua/engine.lua
@@ -26,17 +26,17 @@ local imports = {
 
 local engine = class:create("engine", engine)
 
-function engine.private.inspect(input, show_hidden, limit, level, buffer, visited)
+function engine.private.inspect(input, show_hidden, depth_limit, level, buffer, visited)
     local input_type = imports.type(input)
     show_hidden = (show_hidden and true) or false
-    limit = math.max(1, imports.tonumber(limit) or 10)
+    depth_limit = math.max(1, imports.tonumber(depth_limit) or 10)
     level = math.max(0, imports.tonumber(level) or 0)
     buffer = buffer or table.pack()
     visited = visited or {}
     if input_type ~= "table" then
         local input_types = {["nil"] = true, ["boolean"] = true, ["string"] = true, ["number"] = true}
         table.insert(buffer, ((input_types[input_type] and (((input_type == "string") and string.format("%q", input)) or imports.tostring(input))) or ("<"..imports.tostring(input)..">")).."\n")
-    elseif level > limit then
+    elseif level > depth_limit then
         table.insert(buffer, "{...}\n")
     elseif visited[input] then
         table.insert(buffer, "{<circular>}\n")
@@ -47,7 +47,7 @@ function engine.private.inspect(input, show_hidden, limit, level, buffer, visite
         for k, v in imports.pairs(input) do
             table.insert(buffer, indent..imports.tostring(k)..": ")
             if k ~= "__index" then
-                engine.private.inspect(v, show_hidden, limit, level + 1, buffer, visited)
+                engine.private.inspect(v, show_hidden, depth_limit, level + 1, buffer, visited)
             else
                 table.insert(buffer, "{<__index>}\n")
             end
@@ -56,7 +56,7 @@ function engine.private.inspect(input, show_hidden, limit, level, buffer, visite
             local metadata = imports.getmetatable(input)
             if metadata and not visited[metadata] then
                 table.insert(buffer, indent.."<metatable>: ")
-                engine.private.inspect(metadata, show_hidden, limit, level + 1, buffer, visited)
+                engine.private.inspect(metadata, show_hidden, depth_limit, level + 1, buffer, visited)
             end
         end
         table.insert(buffer, string.rep("  ", level).."}\n")
