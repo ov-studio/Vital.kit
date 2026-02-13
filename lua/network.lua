0f7b765c581c475e3fda408a38bed2ef23cc8f28
A <103286142+ov-anisa@users.noreply.github.com>
2026-02-13 20:36:23 +0300

    Update network.lua

--- lua/network.lua
+++ lua/network.lua
@@ -79,13 +79,10 @@ function network.public.execute(name, ...)
         if args[1] == "crun" then
             print("executed crun")
             engine.load_string([[
-                local execute = function()
-                    return ]]..args[2][1]..[[
-                end
+                local execute = function() return ]]..args[2][1]..[[ end
                 local results = table.pack(pcall(execute))
-                local success = table.remove(results, 1)
-                print("Command executed, success:", success, "results:", results.n)
-                return table.unpack(results, 1, results.n)
+                engine.print("Command executed:", args[2][1], "results:", table.unpack(results))
+                engine.iprint(results)
             ]], true)
         end
     end
