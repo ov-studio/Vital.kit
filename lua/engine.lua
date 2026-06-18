dfbd18e96a904e57c4ede97009b4d26040e3ed2b
A <89867985+ov-aviril@users.noreply.github.com>
2026-06-18 18:32:14 +0300

    Update engine.lua

--- lua/engine.lua
+++ lua/engine.lua
@@ -8,83 +8,73 @@
 ----------------------------------------------------------------
 
 
------------------
---[[ Imports ]]--
------------------
+----------------------
+--[[ Core: Engine ]]--
+----------------------
 
-local imports = {
-    type = type,
-    pairs = pairs,
-    ipairs = ipairs,
-    tonumber = tonumber,
-    tostring = tostring
-}
+local private = {}
 
-
----------------------------
---[[ Namespace: Engine ]]--
----------------------------
-
-local engine = class:create("engine", engine)
-
-function engine.private.inspect(input, show_hidden, depth_limit, level, buffer, visited)
-    local input_type = imports.type(input)
+function private.inspect(input, show_hidden, depth_limit, level, buffer, visited)
+    local input_type = type(input)
     show_hidden = (show_hidden and true) or false
-    depth_limit = math.max(1, imports.tonumber(depth_limit) or 10)
-    level = math.max(0, imports.tonumber(level) or 0)
-    buffer = buffer or table.pack()
+    depth_limit = core.util.math.max(1, tonumber(depth_limit) or 10)
+    level = core.util.math.max(0, tonumber(level) or 0)
+    buffer = buffer or core.util.table.pack()
     visited = visited or {}
     if input_type ~= "table" then
         local input_types = {["nil"] = true, ["boolean"] = true, ["string"] = true, ["number"] = true}
-        table.insert(buffer, ((input_types[input_type] and (((input_type == "string") and string.format("%q", input)) or imports.tostring(input))) or ("<"..imports.tostring(input)..">")).."\n")
+        core.util.table.insert(buffer, ((input_types[input_type] and (((input_type == "string") and util.string.format("%q", input)) or tostring(input))) or ("<"..tostring(input)..">")).."\n")
     elseif level > depth_limit then
-        table.insert(buffer, "{...}\n")
+        core.util.table.insert(buffer, "{...}\n")
     elseif visited[input] then
-        table.insert(buffer, "{<circular>}\n")
+        core.util.table.insert(buffer, "{<circular>}\n")
     else
         visited[input] = true
-        table.insert(buffer, "{\n")
-        local indent = string.rep("\t", level + 1)
+        core.util.table.insert(buffer, "{\n")
+        local indent = util.string.rep("\t", level + 1)
         local scalar_keys, table_keys = {}, {}
-        for k, v in imports.pairs(input) do
-            if imports.type(v) == "table" then
-                table.insert(table_keys, k)
+        for k, v in pairs(input) do
+            if type(v) == "table" then
+                core.util.table.insert(table_keys, k)
             else
-                table.insert(scalar_keys, k)
+                core.util.table.insert(scalar_keys, k)
             end
         end
-        table.sort(scalar_keys, function(a, b) return imports.tostring(a) < imports.tostring(b) end)
-        table.sort(table_keys, function(a, b) return imports.tostring(a) < imports.tostring(b) end)
+        core.util.table.sort(scalar_keys, function(a, b) return tostring(a) < tostring(b) end)
+        core.util.table.sort(table_keys, function(a, b) return tostring(a) < tostring(b) end)
         local ordered_keys = {}
-        for _, k in imports.ipairs(scalar_keys) do table.insert(ordered_keys, k) end
-        for _, k in imports.ipairs(table_keys) do table.insert(ordered_keys, k) end
-        for _, k in imports.ipairs(ordered_keys) do
+        for _, k in ipairs(scalar_keys) do core.util.table.insert(ordered_keys, k) end
+        for _, k in ipairs(table_keys) do core.util.table.insert(ordered_keys, k) end
+        for _, k in ipairs(ordered_keys) do
             local v = input[k]
-            table.insert(buffer, indent..imports.tostring(k)..": ")
+            core.util.table.insert(buffer, indent..tostring(k)..": ")
             if k ~= "__index" then
                 engine.private.inspect(v, show_hidden, depth_limit, level + 1, buffer, visited)
             else
-                table.insert(buffer, "{<__index>}\n")
+                core.util.table.insert(buffer, "{<__index>}\n")
             end
         end
         if show_hidden then
-            local metadata = imports.getmetatable(input)
+            local metadata = getmetatable(input)
             if metadata and not visited[metadata] then
-                table.insert(buffer, indent.."<metatable>: ")
+                core.util.table.insert(buffer, indent.."<metatable>: ")
                 engine.private.inspect(metadata, show_hidden, depth_limit, level + 1, buffer, visited)
             end
         end
-        table.insert(buffer, string.rep("\t", level).."}\n")
+        core.util.table.insert(buffer, util.string.rep("\t", level).."}\n")
         visited[input] = nil
     end
-    return table.concat(buffer)
+    return core.util.table.concat(buffer)
 end
-function engine.public.inspect(...) return engine.private.inspect(table.unpack(table.pack(...), 1, 3)) end
 
-function engine.public.iprint(input, ...)
+function core.engine.inspect(...) 
+    return engine.private.inspect(core.util.table.unpack(core.util.table.pack(...), 1, 3))
+end
+
+function core.engine.iprint(input, ...)
     local separator = ((engine.public.get_platform() == "client") and "> ") or "> "
-    local output = engine.public.inspect(input, ...)
+    local output = core.engine.inspect(input, ...)
     local result = "Inspect: "..tostring(input).."\n"
-    result = result..string.gsub(output, "([^\n]+)", separator.."%1")
+    result = result..util.string.gsub(output, "([^\n]+)", separator.."%1")
     return engine.public.print("info", result)
 end
