e4381270585b4001a805f7b897de5a367b0ac135
A <89867985+ov-aviril@users.noreply.github.com>
2026-06-19 14:36:33 +0300

    -/-

--- lua/engine.lua
+++ lua/engine.lua
@@ -1,6 +1,6 @@
 ----------------------------------------------------------------
 --[[ Resource: Vital.sandbox
-     Script: module: lua: engine.lua
+     Script: engine.lua
      Author: ov-studio
      Developer(s): Aviril, Tron, Mario, Аниса, A-Variakojiene
      DOC: 14/09/2022
