4990189b4dcf5fbad5653b2b97ed5a650ad42d37
A <89867985+ov-aviril@users.noreply.github.com>
2026-06-26 18:43:52 +0300

    Update table.lua

--- lua/util/table.lua
+++ lua/util/table.lua
@@ -8,49 +8,37 @@
 ----------------------------------------------------------------
 
 
------------------
---[[ Imports ]]--
------------------
+---------------------
+--[[ Util: Table ]]--
+---------------------
 
-local imports = {
-    type = type,
-    pairs = pairs,
-    tostring = tostring,
-    tonumber = tonumber,
+local private = {
     rawset = rawset,
     rawget = rawget,
-    unpack = table.unpack,
-    getmetatable = getmetatable,
+    unpack = util.table.unpack,
     json = json
 }
 json = nil
 
-
---------------------------
---[[ Namespace: Table ]]--
---------------------------
-
-local private = {}
-
 function util.table.len(input)
-  return imports.rawget(input, "n") or #input
+  return private.rawget(input, "n") or #input
 end
 
 function util.table.unpack(input, start_at, end_at)
-  return imports.unpack(input, start_at or 1, end_at or util.table.len(input))
+  return private.unpack(input, start_at or 1, end_at or util.table.len(input))
 end
 
 function util.table.insert(input, value, index)
     local n = util.table.len(input)
     if index == nil then
         input[n + 1] = value
-        imports.rawset(input, "n", n + 1)
+        private.rawset(input, "n", n + 1)
     else
         for i = n, index, -1 do
             input[i + 1] = input[i]
         end
         input[index] = value
-        imports.rawset(input, "n", n + 1)
+        private.rawset(input, "n", n + 1)
     end
 end
 
@@ -64,29 +52,29 @@ function util.table.remove(input, index)
         input[i] = input[i + 1]
     end
     input[n] = nil
-    imports.rawset(input, "n", n - 1)
+    private.rawset(input, "n", n - 1)
     return result
 end
 
 function util.table.encode(input, mode, ...)
-    if not input or (imports.type(input) ~= "table") then return false end
+    if not input or (type(input) ~= "table") then return false end
     mode = mode or "JSON"
-    if mode == "JSON" then return imports.json.encode(input, ...) end
+    if mode == "JSON" then return private.json.encode(input, ...) end
     return false
 end
 
 function util.table.decode(input, mode, ...)
-    if not input or (imports.type(input) ~= "string") then return false end
+    if not input or (type(input) ~= "string") then return false end
     mode = mode or "JSON"
-    if mode == "JSON" then return imports.json.decode(input, ...) end
+    if mode == "JSON" then return private.json.decode(input, ...) end
     return false
 end
 
 function util.table.clone(input, recursive)
-    if not input or (imports.type(input) ~= "table") then return false end
+    if not input or (type(input) ~= "table") then return false end
     local result = {}
-    for i, j in imports.pairs(input) do
-        if (imports.type(j) == "table") and recursive then
+    for i, j in pairs(input) do
+        if (type(j) == "table") and recursive then
             result[i] = util.table.clone(j, recursive)
         else
             result[i] = j
