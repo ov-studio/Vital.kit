7f2b60f8d1f8132597814cf02bea6774a816415c
A <89867985+ov-aviril@users.noreply.github.com>
2026-06-27 18:14:00 +0300

    -/-

--- console/src/app/bridge.jsx
+++ console/src/app/bridge.jsx
@@ -1,7 +1,3 @@
-export const DEFAULT_POSITION = { x: 2, y: 2 };
-export const LOG_DEBOUNCE = 3000;
-export const LOG_LIMIT = 500;
-
 const GODOT_KEY = {
   // Printable
   'asciitilde': '~', 'quoteleft': '`', 'exclam': '!', 'quotedbl': '"',
