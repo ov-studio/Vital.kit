1c49a14a9732966c5ba58bbd4bd13855111963e6
A <89867985+ov-aviril@users.noreply.github.com>
2026-07-17 09:02:02 +0300

    Added `js` module with cursor and key enums injected globally

---
+++ js/enum/cursor.js
@@ -0,0 +1,19 @@
+/*----------------------------------------------------------------
+     Resource: Vital.sandbox
+     Script: enum: cursor.js
+     Author: ov-studio
+     Developer(s): Aviril, Tron, Mario, Аниса, A-Variakojiene
+     DOC: 14/09/2022
+     Desc: Cursor Enum
+----------------------------------------------------------------*/
+
+
+///////////////////
+// Enum: Cursor //
+///////////////////
+
+window.CURSOR_MODE = {
+    VISIBLE:  0,
+    HIDDEN:   1,
+    CAPTURED: 2,
+};
---
+++ js/enum/key.js
@@ -0,0 +1,197 @@
+/*----------------------------------------------------------------
+     Resource: Vital.sandbox
+     Script: enum: key.js
+     Author: ov-studio
+     Developer(s): Aviril, Tron, Mario, Аниса, A-Variakojiene
+     DOC: 14/09/2022
+     Desc: Key Enum
+----------------------------------------------------------------*/
+
+
+////////////////
+// Enum: Key //
+////////////////
+
+window.KEY = {
+    // Alphabet
+    A: 65,
+    B: 66,
+    C: 67,
+    D: 68,
+    E: 69,
+    F: 70,
+    G: 71,
+    H: 72,
+    I: 73,
+    J: 74,
+    K: 75,
+    L: 76,
+    M: 77,
+    N: 78,
+    O: 79,
+    P: 80,
+    Q: 81,
+    R: 82,
+    S: 83,
+    T: 84,
+    U: 85,
+    V: 86,
+    W: 87,
+    X: 88,
+    Y: 89,
+    Z: 90,
+
+    // Digits
+    0: 48,
+    1: 49,
+    2: 50,
+    3: 51,
+    4: 52,
+    5: 53,
+    6: 54,
+    7: 55,
+    8: 56,
+    9: 57,
+
+    // Numpad Digits
+    NUM_0: 4194438,
+    NUM_1: 4194429,
+    NUM_2: 4194430,
+    NUM_3: 4194431,
+    NUM_4: 4194432,
+    NUM_5: 4194433,
+    NUM_6: 4194434,
+    NUM_7: 4194435,
+    NUM_8: 4194436,
+    NUM_9: 4194437,
+
+    // Numpad Operators
+    NUM_ADD:      4194441,
+    NUM_SUBTRACT: 4194442,
+    NUM_MULTIPLY: 4194443,
+    NUM_DIVIDE:   4194444,
+    NUM_PERIOD:   4194439,
+
+    // Function Keys
+    F1:  4194332,
+    F2:  4194333,
+    F3:  4194334,
+    F4:  4194335,
+    F5:  4194336,
+    F6:  4194337,
+    F7:  4194338,
+    F8:  4194339,
+    F9:  4194340,
+    F10: 4194341,
+    F11: 4194342,
+    F12: 4194343,
+    F13: 4194344,
+    F14: 4194345,
+    F15: 4194346,
+    F16: 4194347,
+    F17: 4194348,
+    F18: 4194349,
+    F19: 4194350,
+    F20: 4194351,
+    F21: 4194352,
+    F22: 4194353,
+    F23: 4194354,
+    F24: 4194355,
+
+    // Arrow Keys
+    UP:    4194320,
+    DOWN:  4194322,
+    LEFT:  4194319,
+    RIGHT: 4194321,
+
+    // Control Keys
+    SPACE:     32,
+    ENTER:     4194309,
+    KP_ENTER:  4194310,
+    ESCAPE:    4194305,
+    TAB:       4194306,
+    BACKTAB:   4194307,
+    BACKSPACE: 4194308,
+    INSERT:    4194311,
+    DELETE:    4194312,
+    HOME:      4194313,
+    END:       4194314,
+    PAGE_UP:   4194315,
+    PAGE_DOWN: 4194316,
+    PAUSE:     4194325,
+    PRINT:     4194326,
+    CLEAR:     4194327,
+    MENU:      4194370,
+
+    // Modifier Keys
+    SHIFT:  4194325,
+    LSHIFT: 4194325,
+    RSHIFT: 4194325,
+    CTRL:   4194326,
+    LCTRL:  4194326,
+    RCTRL:  4194326,
+    ALT:    4194328,
+    LALT:   4194328,
+    RALT:   4194328,
+    META:   4194329,
+    LMETA:  4194329,
+    RMETA:  4194329,
+
+    // Lock Keys
+    CAPS_LOCK:   4194330,
+    NUM_LOCK:    4194331,
+    SCROLL_LOCK: 4194332,
+
+    // Punctuation & Symbols
+    EXCLAM:        33,
+    QUOTEDBL:      34,
+    NUMBER_SIGN:   35,
+    DOLLAR:        36,
+    PERCENT:       37,
+    AMPERSAND:     38,
+    APOSTROPHE:    39,
+    PAREN_LEFT:    40,
+    PAREN_RIGHT:   41,
+    ASTERISK:      42,
+    PLUS:          43,
+    COMMA:         44,
+    MINUS:         45,
+    PERIOD:        46,
+    SLASH:         47,
+    COLON:         58,
+    SEMICOLON:     59,
+    LESS:          60,
+    EQUAL:         61,
+    GREATER:       62,
+    QUESTION:      63,
+    AT:            64,
+    BRACKET_LEFT:  91,
+    BACKSLASH:     92,
+    BRACKET_RIGHT: 93,
+    CIRCUM:        94,
+    UNDERSCORE:    95,
+    QUOTE_LEFT:    96,
+    BRACE_LEFT:    123,
+    BAR:           124,
+    BRACE_RIGHT:   125,
+    TILDE:         126,
+
+    // Volume & Media Keys
+    VOLUME_DOWN:    4194424,
+    VOLUME_MUTE:    4194423,
+    VOLUME_UP:      4194425,
+    MEDIA_PLAY:     4194426,
+    MEDIA_STOP:     4194427,
+    MEDIA_PREVIOUS: 4194428,
+    MEDIA_NEXT:     4194429,
+    MEDIA_RECORD:   4194430,
+
+    // Mouse Buttons
+    MOUSE_LEFT:       1,
+    MOUSE_RIGHT:      2,
+    MOUSE_MIDDLE:     3,
+    MOUSE_WHEEL_UP:   4,
+    MOUSE_WHEEL_DOWN: 5,
+    MOUSE_XBUTTON1:   8,
+    MOUSE_XBUTTON2:   9
+};
