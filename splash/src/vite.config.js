d4f4b4918c05216b873a87825a8f2b2c2b01e67a
A <234397639+ov-kivi@users.noreply.github.com>
2026-08-14 15:40:57 +0300

    Update vite.config.js

--- splash/src/vite.config.js
+++ splash/src/vite.config.js
@@ -1,6 +1,6 @@
-import * as fs            from 'fs';
-import * as path          from 'path';
-import * as vite          from 'vite';
+import * as fs             from 'fs';
+import * as path           from 'path';
+import * as vite           from 'vite';
 import * as viteSingleFile from 'vite-plugin-singlefile';
 
 const kit_plugin = () => ({
