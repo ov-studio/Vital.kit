cf5bca3d5c0ce5a9bb723a685a616de58ce6fa18
A <234397639+ov-kivi@users.noreply.github.com>
2026-08-26 01:45:02 +0300

    Fix build error

--- console/src/vite.config.js
+++ console/src/vite.config.js
@@ -2,8 +2,11 @@ import * as fs                from 'fs';
 import * as path              from 'path';
 import * as vite              from 'vite';
 import * as react             from '@vitejs/plugin-react';
-import * as vite_singlefile    from 'vite-plugin-singlefile';
+import * as vite_singlefile   from 'vite-plugin-singlefile';
 import * as shared_kit_plugin from '../../shared/kit-plugin.js';
+import { fileURLToPath }      from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default vite.defineConfig({
   plugins: [
@@ -11,6 +14,14 @@ export default vite.defineConfig({
     vite_singlefile.viteSingleFile(),
     shared_kit_plugin.kit_plugin()
   ],
+
+  resolve: {
+    alias: {
+      react: path.resolve(__dirname, 'node_modules/react'),
+      'react-dom': path.resolve(__dirname, 'node_modules/react-dom'),
+    },
+    dedupe: ['react', 'react-dom'],
+  },
 
   build: {
     outDir: '../build',
