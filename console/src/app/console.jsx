c07252e8c1b9a8b73eca8b8e6f390f16e6afbc44
A <234397639+ov-kivi@users.noreply.github.com>
2026-08-26 01:22:41 +0300

    Update console.jsx

--- console/src/app/console.jsx
+++ console/src/app/console.jsx
@@ -4,6 +4,7 @@ import * as app_config           from './config';
 import * as app_util             from './util';
 import * as app_components       from './components';
 import * as shared_ui_iconbutton from '../../../shared/ui/iconbutton/index.jsx';
+import * as shared_ui_tagpill    from '../../../shared/ui/tagpill/index.jsx';
 
 export const Console = () => {
   const [logs, set_logs] = react.useState([]);
